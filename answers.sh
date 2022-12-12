5e601ae0e75a4f1cfe28f47af36c737b35a3ed12
taleb-khaddour <taleb.khaddour.20311@gmail.com>
2022-12-12 21:03:12 +0200

    creat tabel

--- answers.sh
+++ answers.sh
@@ -12,3 +12,28 @@ SET Points=150
 WHERE Name= "Alex";
 
 
+
+Creating Table
+
+
+
+CREATE TABLE "graduates" (
+	"ID"	INTEGER NOT NULL,
+	"Name"	TEXT NOT NULL UNIQUE,
+	"Age"	INTEGER,
+	"Gender"	TEXT,
+	"Points"	INTEGER,
+	"Graduation"	TEXT,
+	PRIMARY KEY("ID" AUTOINCREMENT)
+);
+
+
+
+
+INSERT INTO  graduates VALUES(1,"Layal",18,"F",350," ");
+
+UPDATE graduates
+SET Graduation ="12/12/2022"
+WHERE Name="Layal";
+
+DELETE FROM graduates WHERE Name="Layal";
