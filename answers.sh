5b899c8bdc4a227c3181a7eb1d8c404f9899e649
taleb-khaddour <taleb.khaddour.20311@gmail.com>
2022-12-12 23:02:32 +0200

    Basic Queries

--- answers.sh
+++ answers.sh
@@ -1,3 +1,6 @@
+
+
+
 1 select name from students  ;
 2 select name from students WHERE age>30;
 3 select name from students  WHERE Gender="F" AND age=30;
@@ -13,7 +16,7 @@ WHERE Name= "Alex";
 
 
 
-Creating Table
+###Creating Table
 
 
 
@@ -41,10 +44,22 @@ DELETE FROM graduates WHERE Name="Layal";
 
 
 
-Joins
+###Joins
 
 14-SELECT employees.Name, employees.Company, companies.Date FROM employees JOIN companies ON employees.Company = companies.name;
 15-SELECT employees.NameFROM employees JOIN companies ON employees.Company = companies.name WHERE date<2000;
 16-SELECT companies.Name FROM companies JOIN employees ON companies.name = employees.Company WHERE employees.Role = "Graphic Designer";
+
+
+### Count & Filter
+
+SELECT name,Max(points) FROM students ;
+SELECT Avg(points) FROM students ;
+SELECT COUNT(points) FROM students WHERE points=500;
+SELECT name FROM students WHERE students.name LIKE "%s%";
+SELECT * FROM students ORDER BY students.Points DESC;
+
+
+
 
 
